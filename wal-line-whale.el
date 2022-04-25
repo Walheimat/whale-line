c8aa356334e74d036c398c0893c418e55e0b7600
Walheimat <krister.schuchardt@gmail.com>
2022-04-25 15:34:09 +0200

    feat(segment): whale animation

---
+++ wal-line-whale.el
@@ -0,0 +1,95 @@
+;;; wal-line-whale.el --- Whale animation. -*- lexical-binding: t; -*-
+
+;; Author: Krister Schuchardt <krister.schuchardt@gmail.com>
+;; Keywords: mode-line
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28.1")
+
+;;; Commentary:
+
+;; A simple whale animation.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'wal-line-utils (expand-file-name "wal-line-utils")))
+
+(defvar-local wal-line-flycheck--face nil)
+
+(declare-function wal-line--segment-buffer-name "wal-line.el")
+(declare-function wal-line--spacer "wal-line-utils.el")
+(declare-function wal-line--is-current-window-p "wal-line-utils.el")
+
+;;;; Customization:
+
+(defcustom wal-line-whale-key-frames
+  [
+   "(__.- >{"
+   "(__.' >{"
+   "(__.- >{"
+   "(__., >{"
+   ]
+  "Animation key frames."
+  :group 'wal-line
+  :type '(vector string))
+
+(defcustom wal-line-whale-animation-speed 0.4
+  "Animation speed."
+  :group 'wal-line
+  :type 'float)
+
+;;;; Functionality:
+
+(defvar wal-line-whale--frame-index 0)
+(defvar wal-line-whale--timer nil)
+(defvar wal-line-whale--frame nil)
+
+(defun wal-line-whale--animate ()
+  "Animate the ASCII whale."
+  (let* ((frame (aref wal-line-whale-key-frames wal-line-whale--frame-index))
+         (colored (propertize frame 'face 'wal-line-highlight)))
+    (setq wal-line-whale--frame-index
+          (mod
+           (1+ wal-line-whale--frame-index)
+           (length wal-line-whale-key-frames))
+          wal-line-whale--frame colored)
+    (force-mode-line-update)))
+
+(defun wal-line-whale--segment ()
+  "Show the current animation frame."
+  (unless wal-line-whale--frame
+    (wal-line-whale--animate))
+  (if (wal-line--is-current-window-p)
+      wal-line-whale--frame
+    ""))
+
+;;;; Entrypoint.
+
+(defun wal-line-whale--setup ()
+  "Set up the animated whale."
+  (add-hook 'kill-buffer-hook #'wal-line-whale--teardown nil t)
+  (unless wal-line-whale--timer
+    (setq wal-line-whale--timer (run-with-timer
+                                 0
+                                 wal-line-whale-animation-speed
+                                 #'wal-line-whale--animate))))
+
+(defun wal-line-whale--teardown ()
+  "Clean up the animation."
+  (when wal-line-whale--timer
+    (cancel-timer wal-line-whale--timer)
+    (setq wal-line-whale--timer nil))
+  (remove-hook 'kill-buffer-hook #'wal-line-whale--teardown t))
+
+(add-hook 'wal-line-setup-hook #'wal-line-whale--setup)
+(add-hook 'wal-line-teardown-hook #'wal-line-whale--teardown)
+
+(defvar wal-line--right-side)
+(wal-line-add-segment
+ wal-line-whale--segment
+ 'right
+ 'wal-line--segment-position)
+
+(provide 'wal-line-whale)
+
+;;; wal-line-whale.el ends here
