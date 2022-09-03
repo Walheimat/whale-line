d6215b98ce5907304d4895b8151970c454aeacdb
Walheimat <krister.schuchardt@gmail.com>
2022-09-03 18:01:01 +0200

    improve(org): show root heading

--- wal-line-org.el
+++ wal-line-org.el
@@ -31,29 +31,64 @@
   :group 'wal-line
   :type 'string)
 
+(defcustom wal-line-org-include 'current-and-root
+  "The heading depth to show."
+  :group 'wal-line
+  :type '(choice (const current-and-root)
+                 (const current)))
+
+(defcustom wal-line-org-max-heading-length 8
+  "The max length of a heading before truncation."
+  :group 'wal-line
+  :type 'integer)
+
 ;;;; Functionality:
 
-(defun wal-line-org--get-heading ()
-  "Get propertized heading."
+(defun wal-line-org--maybe-truncate (heading)
+  "Maybe truncate HEADING."
+  (let ((len (string-width heading)))
+    (if (> len wal-line-org-max-heading-length)
+        (concat (substring heading 0 (max (- len 3) 1)) "...")
+      heading)))
+
+(defun wal-line-org--get-next-heading ()
+  "Get the next heading going backwards."
+  (org-back-to-heading)
+  (let* ((components (org-heading-components))
+         (level (nth 0 components))
+         (face (nth (- level 1) org-level-faces))
+         (heading (org-link-display-format (nth 4 components))))
+    (propertize heading 'face face)))
+
+(defun wal-line-org--collect-headings ()
+  "Collect headings until it's no longer safe."
+  (save-excursion
+    (cl-loop collect (wal-line-org--get-next-heading)
+             while (org-up-heading-safe))))
+
+(defun wal-line-org--build-segment ()
+  "Build the segment from included segments."
   (org-with-wide-buffer
    (goto-char (window-start))
-   (org-back-to-heading)
-   (let* ((components (org-heading-components))
-          (level (nth 0 components))
-          (face (nth (- level 1) org-level-faces))
-          (heading (org-link-display-format (nth 4 components))))
-     (propertize heading 'face face))))
+   (let ((headings (wal-line-org--collect-headings)))
+     (if (null headings)
+         ""
+       (pcase wal-line-org-include
+         ('current (nth 0 headings))
+         ('current-and-root
+          (if (> (length headings) 1)
+              (concat
+               (wal-line-org--maybe-truncate (car (last headings)))
+               (wal-line--spacer)
+               (nth 0 headings))
+            (nth 0 headings))))))))
 
 (defun wal-line-org--segment ()
   "Displays the current heading."
   (if (eq major-mode 'org-mode)
-      (if (org-before-first-heading-p)
-          ""
-        (concat (wal-line--spacer)
-                wal-line-org-delimiter
-                (wal-line--spacer)
-                (wal-line-org--get-heading)
-                (wal-line--spacer)))
+      (concat
+       wal-line-org-delimiter
+       (wal-line-org--build-segment))
     ""))
 
 (defvar wal-line--segments)
