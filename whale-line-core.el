106be334cf4f3975ae9bb13035a0ba5659410457
Walheimat <krister.schuchardt@gmail.com>
2023-08-18 08:36:25 +0200

    release: v0.7.0

--- whale-line-core.el
+++ whale-line-core.el
@@ -2,7 +2,7 @@
 
 ;; Author: Krister Schuchardt <krister.schuchardt@gmail.com>
 ;; Homepage: https://github.com/Walheimat/whale-line
-;; Version: 0.6.2
+;; Version: 0.7.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces mode-line
 
